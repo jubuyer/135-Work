4468a29c7e709e3e82d47200902ffccb3dc54b14
jubayer <jubayer7a@gmail.com>
2021-09-27 09:03:46 -0400

    in class code Sep 27

--- code/testing.cpp
+++ code/testing.cpp
@@ -4,8 +4,13 @@
 #include <cstdio>
 #include <iostream>
 
-#define LOG(x) = std::cerr << x << "\n";
+#define LOG(x) std::cerr << x << "\n";
 
 int main () {
-	
+	std::cout << "Start of program\n";
+	LOG("this is going to cerr");
+	std::cout << "this is cout\n";
+	abort();
+
+	return 0;
 }
