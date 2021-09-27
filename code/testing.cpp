5e7a1d0d5c33f2e7f7f2ee9f67dfd4fc95120d3b
jubayer <jubayer7a@gmail.com>
2021-09-27 09:09:56 -0400

    in class code Sep 27

--- code/testing.cpp
+++ code/testing.cpp
@@ -1,16 +1,29 @@
 //September 27, 2021
 //ways to test code
 
+/*
+ - cerr does not buffer and will print immediately 
+ - cout has a buffer and theres a chance that it will not print due to crash
+*/
+
 #include <cstdio>
 #include <iostream>
 
-#define LOG(x) std::cerr << x << "\n";
+#define LOGGING
+
+#ifdef LOGGING
+	#define LOG(x) std::cerr << x << "\n"; 
+#else
+	#define LOG(x)
+#endif
 
 int main () {
 	std::cout << "Start of program\n";
 	LOG("this is going to cerr");
 	std::cout << "this is cout\n";
-	abort();
+
+	#undef LOGGING
+	
 
 	return 0;
 }
