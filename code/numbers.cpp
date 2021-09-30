cf2fd4b94a1d38164c415d40083ec027043c29c3
jubuyer <jahmed10@stuy.edu>
2021-09-29 22:08:01 -0400

    Created and tested nextPrime, need to find better implementation

--- code/numbers.cpp
+++ code/numbers.cpp
@@ -17,16 +17,21 @@ bool isDivisbleby(int n, int d) {
 	return divisble;
 }
 
+//Task B
 bool isPrime(int n) {
 	std::string error = "Please enter a number greater than 1. ";
 	int divisble = 0;
 	bool prime;
 	int tester = n;
-	if (n <= 1) {
+	//Checks if input will ruin code
+	if (n < 1) {
 	 	cout << error;
 	 	return false;
- 	}
+ 	} else if (n == 1) {
+		return false;
+	}
 
+	//Tests if n is divisble by any number all the way to 1
 	while (tester > 1) {
 		if (isDivisbleby(n, tester-1)) {
 			divisble++;
@@ -39,24 +44,50 @@ bool isPrime(int n) {
 	return prime;
 }
 
+//Task C
+int nextPrime(int n) {
+	int temp = 1;
+	int next = 0;
+
+	while(true) {
+		if (isPrime(n + temp)) {
+			next = n + temp;
+			return next;
+		} else {
+			temp++;
+		}
+	}
+
+	return next;
+}
+
 int main () {
-	cout << "-----------TESTS FOR TASK A-----------\n";
-	cout << "Test with 4 and 2: ";
-	(isDivisbleby(4,2) ? cout << "Divisble\n" : cout << "Not divisible\n");
-	cout << "Test with 4 and 0: ";
-	(isDivisbleby(4,0) ? cout << "Divisble\n" : cout << "Not divisible\n");
-	cout << "Test with 4 and 1: ";
-	(isDivisbleby(4,1) ? cout << "Divisble\n" : cout << "Not divisible\n");
+	// cout << "-----------TESTS FOR TASK A-----------\n";
+	// cout << "Test with 4 and 2: ";
+	// (isDivisbleby(4,2) ? cout << "Divisble\n" : cout << "Not divisible\n");
+	// cout << "Test with 4 and 0: ";
+	// (isDivisbleby(4,0) ? cout << "Divisble\n" : cout << "Not divisible\n");
+	// cout << "Test with 4 and 1: ";
+	// (isDivisbleby(4,1) ? cout << "Divisble\n" : cout << "Not divisible\n");
+	//
+	// cout << "-----------TESTS FOR TASK B-----------\n";
+	// cout << "Test with 1: ";
+	// (isPrime(1) ? cout << "Prime\n" : cout << "Not Prime\n");
+	// cout << "Test with 14: ";
+	// (isPrime(14) ? cout << "Prime\n" : cout << "Not Prime\n");
+	// cout << "Test with 21: ";
+	// (isPrime(21) ? cout << "Prime\n" : cout << "Not Prime\n");
+	// cout << "Test with 41: ";
+	// (isPrime(41) ? cout << "Prime\n" : cout << "Not Prime\n");
 
-	cout << "-----------TESTS FOR TASK B-----------\n";
-	cout << "Test with 2: ";
-	(isPrime(2) ? cout << "Prime\n" : cout << "Not Prime\n");
-	cout << "Test with 4: ";
-	(isPrime(4) ? cout << "Prime\n" : cout << "Not Prime\n");
-	cout << "Test with 2: ";
-	(isPrime(7) ? cout << "Prime\n" : cout << "Not Prime\n");
-	cout << "Test with 1: ";
-	(isPrime(1) ? cout << "Prime\n" : cout << "Not Prime\n");
-
+	cout << "-----------TESTS FOR TASK C-----------\n";
+	cout << "\nTest with 2: ";
+	cout << "\nThe prime after 2 is " << nextPrime(2);
+	cout << "\nTest with 19: ";
+	cout << "\nThe prime after 19 is " << nextPrime(19);
+	cout << "\nTest with 20: ";
+	cout << "\nThe prime after 20 is " << nextPrime(20);
+	cout << "\nTest with 41: ";
+	cout << "\nThe prime after 41 is " << nextPrime(41) << "\n";
 	return 0;
 }
